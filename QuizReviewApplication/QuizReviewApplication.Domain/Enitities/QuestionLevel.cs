a5efb03bdbf26f9cb4ab77a4d4d2e5ee42c889ed
BhuGrg <bhupendragurung263@gmail.com>
2024-03-06 22:23:55 +0000

    Adding entites for Question Model

---
+++ QuizReviewApplication/QuizReviewApplication.Domain/Enitities/QuestionLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizReviewApplication.Domain.Enitities
+{
+    //Question is divided into different level
+    // Easy, Medium and Hard
+    public class QuestionLevel
+    {
+        public int Id { get; set; }
+        public string Level { get; set; } = "Easy";
+        public int QuestionId {get;set;}    
+        public Question Questions { get; set; }
+    }
+}
