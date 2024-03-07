c2a352ff53c84d05a66cf7f786f52bc864e8bc0c
Catalin Rosu <rosucatalin@yahoo.com>
2024-03-07 21:35:00 +0000

    add question entities

---
+++ QuizReviewApplication/QuizReviewApplication.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizReviewApplication.Domain.Entities
+{
+    // Each Question belong to one or more category 
+    // For example one question belong to c# and .Net Category
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public ICollection<Question> Questions { get; set; }
+    }
+}
