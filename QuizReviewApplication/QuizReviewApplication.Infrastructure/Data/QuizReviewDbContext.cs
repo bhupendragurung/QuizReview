ca2245395eebe4afc4801b4b08a6061fc0abbdc0
BhuGrg <bhupendragurung263@gmail.com>
2024-03-07 09:16:11 +0000

    changing entities spelling and put .vs file in gitignore

---
+++ QuizReviewApplication/QuizReviewApplication.Infrastructure/Data/QuizReviewDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizReviewApplication.Infrastructure.Data
+{
+    public class QuizReviewDbContext:DbContext
+    {
+        public QuizReviewDbContext(DbContextOptions<QuizReviewDbContext> options):base(options)
+        {
+                
+        }
+
+    }
+}
